51081dcbcabeeb40e758c69d708d99cb2ee900c3
Rostyslav Vatskus <rostikvac12@gmail.com>
2020-04-22 19:51:31 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let years = +prompt("Скільки вам років?", " ");
+alert(`Привіт мені - ${years} років!`);
+
+// // 2
+
+// const thisYear = 2020;
+// let year = +prompt("Якого року ви народження?", "");
+// alert(`Вам ${thisYear - year} років! `);
+
+// // 3
+// let sideA = +prompt("Довжина сторони 'а' прямокутника?", " ");
+// let sideB = +prompt("Довжина сторони 'b' прямокутника?", " ");
+// let perimeter = 2*(sideA + sideB);
+// alert(`Периметр прямокутника ${perimeter}`);
+
+// // 4
+
+// let radius = +prompt("Радіус кола?", " ");
+// let area = 3.14*(radius * radius);
+// alert(`Площа кола ${area}.`);
+
+// /*5*/
+
+// let kmHour = 50;
+// let restKm = +prompt("Скільки часу ще залишилося їхати?", " ");
+// let distance = 50 * restKm;
+// alert(`Відстань яку залишилося проїхати ${distance}км.`);
+
+// // 6
+// let miles = +prompt("Милі?", "");
+// alert(`Кілометрів ${miles * 1609}`);
+// let km = +prompt("Кілометрів?", "");
+// alert(`Миль ${km / 1609}`);
+
+// /*7*/
+
+// let money = +prompt("Коштів на картці?", " ");
+// let priceGasoline = +prompt("Ціна 1 літр бензина", " ");
+// let literGasoline = money / priceGasoline;
+// alert(`Ви можете купити ${literGasoline} літрів бензину!`)
+// let remainderMoney = money % priceGasoline;
+// alert(`Залишок грошей ${remainderMoney} грн.`);
